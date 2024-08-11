0cd9283af485ee817458ca46756cfee7663c63b6
Puneet Chauhan <87594848+puneet7803@users.noreply.github.com>
2024-08-11 16:08:50 +0530

    Add files via upload

---
+++ stone scissor paper/script.js
@@ -0,0 +1,60 @@
+let userScore = 0;
+let compScore = 0;
+
+const choices = document.querySelectorAll(".choice");
+
+const msg = document.querySelector("#msg");
+const userScorePara = document.querySelector("#user-score");
+const compScorePara = document.querySelector("#comp-score");
+
+const genCompChoice = () => {
+    const options = ["rock", "paper", "scissors"];
+    const randIdx = Math.floor(Math.random() * 3);
+    return options[randIdx];
+};
+
+const drawGame = () => {
+    msg.innerText = "Game was a Draw!";
+    msg.style.backgroundColor = "#081b31";
+};
+
+const showWinner = (userWin, userChoice, compChoice) => {
+    if (userWin) {
+        userScore++;
+        userScorePara.innerText = userScore;
+        msg.innerText = `You Win! ${userChoice} beats ${compChoice}`;
+        msg.style.backgroundColor = "green";
+    } else {
+        compScore++;
+        compScorePara.innerText = compScore;
+        msg.innerText = `You lost! ${compChoice} beats ${userChoice}`;
+        msg.style.backgroundColor = "red";
+    }
+};
+
+const playGame = (userChoice) => {
+    const compChoice = genCompChoice();
+
+    if (userChoice === compChoice) {
+        drawGame();
+    } else {
+        let userWin = false;
+
+        if (userChoice === "rock" && compChoice === "scissors") {
+            userWin = true;
+        } else if (userChoice === "paper" && compChoice === "rock") {
+            userWin = true;
+        } else if (userChoice === "scissors" && compChoice === "paper") {
+            userWin = true;
+        }
+
+        showWinner(userWin, userChoice, compChoice);
+    }
+};
+
+choices.forEach((choice) => {
+    choice.addEventListener("click", () => {
+        const userChoice = choice.getAttribute("id");
+        playGame(userChoice);
+    });
+});
